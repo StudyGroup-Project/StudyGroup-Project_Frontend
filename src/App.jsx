037e89d66ab0d93ac63295a7859b8c9d5f914072
gyunii <164734784+gyunii@users.noreply.github.com>
2025-11-15 20:03:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -113,10 +113,7 @@ function App() {
         <Route path='assignmentstest' element={<AssignmentsTest/>}/>
         <Route path='assignmentstestlist' element={<AssignmentsTestList/>}/>
         <Route path='notice' element={<NoticeScreen/>}/>
-<<<<<<< HEAD
-=======
         <Route path='groupScreenhost/:id' element={<GroupScreenHost/>}/>
->>>>>>> upstream/main
         <Route path='noticedetail' element={<NoticeDetail/>}/>
         <Route path='noticedetailhost' element={<NoticeDetailHost/>}/>
         <Route path='noticehost' element={<NoticeHost/>}/>
